956f0320cef099a3354fc1b4438e5bd6b52c37d8
Gallardo1204 <magb1204@gmail.com>
2023-03-23 02:06:45 -0500

    Configuraciones iniciales

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192, // tamaño máximo en bytes de la imagen
+              name: 'img/[name].[ext]' // ruta de salida de la imagen
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'src/css/styles.css',
+    }),
+  ],
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
